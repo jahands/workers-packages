2e74bfc791f1177ec9bc5cccb91736d2fb4dae4b
Jacob Hands <jacob@gogit.io>
2024-09-17 05:52:19 -0500

    Fix workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs'
 import { parse } from 'yaml'
 
-const pnpmWorkspace = parse(fs.readFileSync('./pnpm-workspace.yaml').toString())
+const pnpmWorkspace = parse(fs.readFileSync('./pnpm-workspace.yaml').toString()).packages
 export default pnpmWorkspace
