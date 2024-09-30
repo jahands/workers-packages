67dce4f55fffdad6e3368468c5a7ec6db0f97aca
Jacob Hands <jacob@gogit.io>
2024-09-30 12:58:42 -0500

    Simplify build script a bit more

--- packages/otel-cf-workers/scripts/build.ts
+++ packages/otel-cf-workers/scripts/build.ts
@@ -1,8 +1,7 @@
 import 'zx/globals'
 
 async function build(name: string): Promise<void> {
-	const scriptPath = `./scripts/${name}`
-	await $`bun ${scriptPath}`
+	await $`bun ./scripts/${name}`
 }
 
 await fs.rm('./dist/', { force: true, recursive: true })
