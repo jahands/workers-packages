7268a6ebc67a7355b33023102cd7842c547e8dbd
Jacob Hands <jacob@gogit.io>
2024-09-30 12:54:54 -0500

    chore: simplify esbuild script

---
+++ .changeset/sixty-insects-agree.md
@@ -0,0 +1,5 @@
+---
+'@jahands/otel-cf-workers': patch
+---
+
+chore: simplify esbuild script
--- packages/otel-cf-workers/scripts/build-lib.ts
+++ packages/otel-cf-workers/scripts/build-lib.ts
@@ -1,25 +1,21 @@
 import * as esbuild from 'esbuild'
 
-async function build(entrypoint: string): Promise<void> {
-	await esbuild.build({
-		entryPoints: [`./src/${entrypoint}`],
-		outdir: './dist/',
-		logLevel: 'warning',
-		outExtension: {
-			'.js': '.mjs',
-		},
-		target: 'es2022',
-		// platform: 'browser', // Doesn't seem to be needed?
-		format: 'esm',
-		bundle: true,
-		treeShaking: true,
-		external: ['node:events', 'node:async_hooks', 'node:buffer'],
-	})
-}
-
-await Promise.all([
-	// entrypoints
-	build('index.ts'),
-	build('otel-cf-workers.ts'),
-	build('opentelemetry-api.ts'),
-])
+await esbuild.build({
+	entryPoints: [
+		// entrypoints
+		'./src/index.ts',
+		'./src/otel-cf-workers.ts',
+		'./src/opentelemetry-api.ts',
+	],
+	outdir: './dist/',
+	logLevel: 'warning',
+	outExtension: {
+		'.js': '.mjs',
+	},
+	target: 'es2022',
+	// platform: 'browser', // Doesn't seem to be needed?
+	format: 'esm',
+	bundle: true,
+	treeShaking: true,
+	external: ['node:events', 'node:async_hooks', 'node:buffer'],
+})
