748bd882f2825c25026dda51504110a258aa4222
Jacob Hands <jacob@gogit.io>
2024-09-30 10:27:01 -0500

    fix: Use tsx instead of bun in build scripts

---
+++ .changeset/nine-horses-complain.md
@@ -0,0 +1,5 @@
+---
+'@jahands/otel-cf-workers': patch
+---
+
+fix: Use tsx instead of bun in build scripts
--- packages/otel-cf-workers/scripts/build.ts
+++ packages/otel-cf-workers/scripts/build.ts
@@ -2,7 +2,7 @@ import 'zx/globals'
 
 async function build(name: string): Promise<void> {
 	const scriptPath = `./scripts/${name}`
-	await $`bun ${scriptPath}`
+	await $`tsx ${scriptPath}`
 }
 
 await fs.rm('./dist/', { force: true, recursive: true })
