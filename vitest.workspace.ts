a75e60d907803529e76a1d69e37584e37abaf2e9
Jacob Hands <jacob@jacobhands.com>
2025-04-13 14:09:13 -0500

    chore: add tests/ to workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,5 +1,3 @@
-import fs from 'node:fs'
-import { parse } from 'yaml'
+import { defineWorkspace } from 'vitest/config'
 
-const pnpmWorkspace = parse(fs.readFileSync('./pnpm-workspace.yaml').toString()).packages
-export default pnpmWorkspace
+export default defineWorkspace(['{examples,packages,tests}/*/vitest.config{,.node}.ts'])
