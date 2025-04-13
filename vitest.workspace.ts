c29b25a19f3db423f602fa1c9dae184a6c26cbb0
Jacob Hands <jacob@jacobhands.com>
2025-04-13 14:15:16 -0500

    fix workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,6 @@
 import { defineWorkspace } from 'vitest/config'
 
-export default defineWorkspace(['{examples,packages,tests}/*/vitest.config{,.node}.ts'])
+export default defineWorkspace([
+	'{examples,packages}/*/vitest.config{,.node}.ts',
+	'tests/*/*/vitest.config{,.node}.ts',
+])
