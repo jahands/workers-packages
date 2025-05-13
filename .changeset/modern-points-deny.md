16200103e81e03bb505070cea6fb70df535b1100
Jacob Hands <jacob@jacobhands.com>
2025-05-13 06:02:46 -0500

    fix: actually run typechecks

---
+++ .changeset/modern-points-deny.md
@@ -0,0 +1,8 @@
+---
+'workers-tagged-logger': patch
+'@jahands/dagger-helpers': patch
+'http-codex': patch
+'@jahands/cli-tools': patch
+---
+
+fix: actually run typechecks
--- packages/dagger-helpers/src/index.ts
+++ packages/dagger-helpers/src/index.ts
@@ -3,7 +3,7 @@ export { constants } from './constants.js'
 export { ParamsToEnv, envStorage } from './env.js'
 export type { EnvContext } from './env.js'
 
-export { sh, shell } from './shell.js'
+export { shell } from './shell.js'
 
 export { getModulePath } from './path.js'
 
