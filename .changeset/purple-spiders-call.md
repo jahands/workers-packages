8768f1bd182cbde08c35ce4067e3d83be0954c37
Jacob Hands <jacob@jacobhands.com>
2025-05-13 04:25:50 -0500

    feat: add set -eu and set -euo pipefail to shell() for sh and bash/zsh respectively

---
+++ .changeset/purple-spiders-call.md
@@ -0,0 +1,5 @@
+---
+'@jahands/dagger-helpers': minor
+---
+
+feat: add set -eu and set -euo pipefail to shell() for sh and bash/zsh respectively
--- packages/dagger-helpers/src/shell.ts
+++ packages/dagger-helpers/src/shell.ts
@@ -1,25 +1,4 @@
-/**
- * Run command with bash
- *
- * @deprecated use shell() instead
- *
- * @example
- *
- * ```ts
- * .withExec(sh('echo hello world!'))
- * ```
- */
-export function sh(input: string | string[]): string[] {
-	const inputAr = Array.isArray(input) ? input : [input]
-	const trimmedInput = inputAr.map((i) =>
-		i
-			.trim()
-			.split('\n')
-			.map((l) => l.trim())
-			.join('\n')
-	)
-	return ['bash', '-c', trimmedInput].flat()
-}
+import { match } from 'ts-pattern'
 
 /**
  * Create a new shell helper with the given shell type
@@ -43,6 +22,11 @@ export function shell(shellName: 'sh' | 'bash' | 'zsh') {
 				.map((l) => l.trim())
 				.join('\n')
 		)
-		return [shellName, '-c', trimmedInput].flat()
+
+		return match(shellName)
+			.with('sh', () => ['sh', '-c', `set -eu; ${trimmedInput}`])
+			.with('bash', () => ['bash', '-c', `set -euo pipefail; ${trimmedInput}`])
+			.with('zsh', () => ['zsh', '-c', `set -euo pipefail; ${trimmedInput}`])
+			.exhaustive()
 	}
 }
