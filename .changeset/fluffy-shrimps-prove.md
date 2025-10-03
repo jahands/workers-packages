7893d9be7bb53ce9af703a06ff54b4dcadcb37b6
Jacob Hands <jacob@jacobhands.com>
2025-10-02 19:14:27 -0500

    feat: add prefix override to shell()

---
+++ .changeset/fluffy-shrimps-prove.md
@@ -0,0 +1,5 @@
+---
+'@jahands/dagger-helpers': minor
+---
+
+feat: add prefix override to shell()
--- packages/dagger-helpers/src/shell.ts
+++ packages/dagger-helpers/src/shell.ts
@@ -1,6 +1,24 @@
 import { match } from 'ts-pattern'
 
 /**
+ * Shell options for a given command
+ */
+export type ShellOptions = {
+	/**
+	 * Prefix to add to all commands.
+	 *
+	 * @default
+	 * ```sh
+	 * # bash, zsh
+	 * set -euo pipefail;
+	 * # sh
+	 * set -eu;
+	 * ```
+	 */
+	prefix?: string
+}
+
+/**
  * Create a new shell helper with the given shell type
  *
  * @param shellName - The name of the shell to use
@@ -15,7 +33,7 @@ import { match } from 'ts-pattern'
  * ```
  */
 export function shell(shellName: 'sh' | 'bash' | 'zsh') {
-	return (input: string | string[]): string[] => {
+	return (input: string | string[], options?: ShellOptions): string[] => {
 		const inputAr = Array.isArray(input) ? input : [input]
 		const trimmedInput = inputAr.map((i) =>
 			i
@@ -25,10 +43,19 @@ export function shell(shellName: 'sh' | 'bash' | 'zsh') {
 				.join('\n')
 		)
 
+		let prefix = match(shellName)
+			.with('bash', 'zsh', () => options?.prefix || 'set -euo pipefail')
+			.with('sh', () => options?.prefix || 'set -eu')
+			.exhaustive()
+			.trim()
+
+		if (!prefix.endsWith(';')) {
+			prefix += ';'
+		}
+
 		return match(shellName)
-			.with('sh', () => ['sh', '-c', `set -eu; ${trimmedInput}`])
-			.with('bash', () => ['bash', '-c', `set -euo pipefail; ${trimmedInput}`])
-			.with('zsh', () => ['zsh', '-c', `set -euo pipefail; ${trimmedInput}`])
+			.with('sh', () => ['sh', '-c', `${prefix} ${trimmedInput}`])
+			.with('bash', 'zsh', () => ['zsh', '-c', `${prefix} ${trimmedInput}`])
 			.exhaustive()
 	}
 }
