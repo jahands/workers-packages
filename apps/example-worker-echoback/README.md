d92050b2eca3e0c7dd5a660bd8855d6ea3c81f46
Jacob Hands <jacob@jacobhands.com>
2025-10-11 11:21:29 -0500

    WIP more stuff

--- apps/example-worker-echoback/README.md
+++
@@ -1,3 +0,0 @@
-# example-worker-echoback
-
-A simple example Worker. New Workers should use .eslintrc.cjs and tsconfig.json similar to the one here.
--- apps/example-worker-echoback/env.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-imports */
-type LocalEnv = import('./src/context').Env
-
-// Add Env to Cloudflare namespace so that we can access it via
-// import { env } from 'cloudflare:workers'
-declare namespace Cloudflare {
-	interface Env extends LocalEnv {}
-}
