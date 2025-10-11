b2ce79169aa82f357a6c54992f0c5c01d2736743
Jacob Hands <jacob@jacobhands.com>
2025-10-11 12:52:07 -0500

    fix: pipe correctly

--- packages/tools/src/cmd/shfmt.cmd.ts
+++ packages/tools/src/cmd/shfmt.cmd.ts
@@ -40,14 +40,14 @@ type Mode = 'write' | 'diff'
 
 async function runShfmt(mode: Mode) {
 	await Promise.all([
-		$`rg --files-with-matches '^#!.*\\b(sh|bash|zsh|fish|dash|ksh|csh)\\b' -g '!*.*' .`.pipe(
-			$`xargs shfmt --case-indent ${`--${mode}`}`.pipe(process.stderr)
-		),
+		$`rg --files-with-matches '^#!.*\\b(sh|bash|zsh|fish|dash|ksh|csh)\\b' -g '!*.*' .`
+			.pipe($`xargs shfmt --case-indent ${`--${mode}`}`)
+			.pipe(process.stderr),
 		$({
 			nothrow: true, // may not be any .sh files
-		})`rg --files-with-matches '^#!.*\\b(sh|bash|zsh|fish|dash|ksh|csh)\\b' -g '*.sh' .`.pipe(
-			$`xargs shfmt --case-indent ${`--${mode}`}`.pipe(process.stderr)
-		),
+		})`rg --files-with-matches '^#!.*\\b(sh|bash|zsh|fish|dash|ksh|csh)\\b' -g '*.sh' .`
+			.pipe($`xargs shfmt --case-indent ${`--${mode}`}`)
+			.pipe(process.stderr),
 	])
 }
 
