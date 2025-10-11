222689feaaedefd217854d1fa5b3f5ea1812daa7
Jacob Hands <jacob@jacobhands.com>
2025-10-11 11:20:24 -0500

    WIP update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { glob } from '@repo/workspace-dependencies/zx'
 
 export default defineConfig(async () => {
 	// All vitest projects
-	const projectConfigPaths = await glob(['{apps,packages}/*/vitest.config{,.node}.ts'])
+	const projectConfigPaths = await glob(['{apps,packages,test}/*/vitest.config{,.node}.ts'])
 
 	return {
 		test: {
