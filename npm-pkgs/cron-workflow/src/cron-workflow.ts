e51e29c03d0d02dc7069df0095f571f2d6a2e975
Jacob Hands <jacob@gogit.io>
2025-10-15 03:38:09 -0500

    WIP getting there

--- npm-pkgs/cron-workflow/src/cron-workflow.ts
+++ npm-pkgs/cron-workflow/src/cron-workflow.ts
@@ -3,23 +3,38 @@ import { WorkflowEntrypoint } from 'cloudflare:workers'
 import type { WorkflowEvent, WorkflowStep } from 'cloudflare:workers'
 
 export class CronWorkflow extends WorkflowEntrypoint {
+	constructor(env: Env, ctx: ExecutionContext) {
+		super(env, ctx)
+
+		// Prevent subclasses from overriding run()
+		if (this.run !== CronWorkflow.prototype.run) {
+			throw new Error(
+				'Cannot override run() method in CronWorkflow. ' +
+				'Implement your cron logic in a different method instead.'
+			)
+		}
+	}
+
 	override async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
-		const getWorkflowBinding = () => {
-			if (!(typeof this.env === 'object')) {
+		const getWorkflowBinding = (): Workflow => {
+			const env = this.env as Record<string, unknown>
+
+			if (!(typeof env === 'object')) {
 				throw new Error('this.env is not an object') // should never happen
 			}
-			if (this.env === null) {
+			if (env === null) {
 				throw new Error('this.env is null') // should never happen
 			}
 
 			// name of the derived class
-			const name = this.constructor.name
-			if (!(name in this.env)) {
+			const className = this.constructor.name
+			if (!(className in env || typeof (this.env as any)[className] === 'undefined')) {
 				throw new Error(
-					`could not find Workflows binding for ${name} - did you add it to your Workflows configuration in wrangler.jsonc?`
+					`could not find Workflows binding for ${className} - did you add it to your Workflows configuration in wrangler.jsonc?`
 				)
 			}
-			if(!(typeof this.env[name] === 'object'))
+
+			return env[className] as Workflow
 		}
 
 		const nextRunTime = await step.do('start time', async () => {
@@ -36,12 +51,13 @@ export class CronWorkflow extends WorkflowEntrypoint {
 				const sleepSize = await step.waitForEvent<{ newSleepSize: number }>('timing-decreased', {
 					type: 'timing-decreased',
 					timeout: nextRunTime,
-				}) // Something will have to emit this event (this might also prove to be  challenging)
+				}) // Something will have to emit this event (this might also prove to be challenging)
 
 				await step.sleepUntil('sleep some more', nextRunTime + sleepSize.payload.newSleepSize)
 			} finally {
 				await step.do('create-next-instance', async () => {
-					await this.env.CRON_WORKFLOW.create({
+					const workflow = getWorkflowBinding()
+					await workflow.create({
 						params: { timeToNextRun: event.payload.timeToNextRun },
 					})
 					return
