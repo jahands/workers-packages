40e8036ad20ae48f9fc4101f2882605744add657
Jacob Hands <jacob@jacobhands.com>
2025-10-17 08:58:10 -0500

    WIP fmt

--- test/cron-workflow-test/src/test/integration/harness.ts
+++ test/cron-workflow-test/src/test/integration/harness.ts
@@ -4,10 +4,13 @@ let lastId = 0
 export async function setupTest() {
 	const id = `instance-${lastId++}`
 	const instance = await introspectWorkflowInstance(env.BasicCron, id)
+
 	await instance.modify(async (m) => {
 		await m.disableSleeps()
 	})
+
 	await env.BasicCron.create({ id })
+
 	return {
 		id,
 		instance,
