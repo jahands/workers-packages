b3de5c8ec86776d634700b18287bc63d9cc4c7e0
Jacob Hands <jacob@jacobhands.com>
2025-10-17 08:44:40 -0500

    WIP test structure

--- examples/cron-worker-example/src/test/integration/cron_workflow.test.ts
+++ examples/cron-worker-example/src/test/integration/cron_workflow.test.ts
@@ -2,18 +2,20 @@ import { describe, it, assert, expect, test } from 'vitest'
 import { env, introspectWorkflowInstance } from 'cloudflare:test'
 import { steps } from './steps'
 
-it('should disable all sleeps, mock an event and complete', async () => {
-	const id = '123456'
-	await using instance = await introspectWorkflowInstance(env.UuidRocksCheckerCron, id)
-	await instance.modify(async (m) => {
-		await m.disableSleeps()
-	})
-
-	await env.UuidRocksCheckerCron.create({ id })
-
-	await expect(
-		instance.waitForStepResult({
-			name: steps.getStartTime,
+describe('UuidRocksCheckerCron', async () => {
+	it('should get start time', async () => {
+		const id = '123456'
+		await using instance = await introspectWorkflowInstance(env.UuidRocksCheckerCron, id)
+		await instance.modify(async (m) => {
+			await m.disableSleeps()
 		})
-	).resolves.toBeGreaterThan(0)
+
+		await env.UuidRocksCheckerCron.create({ id })
+
+		await expect(
+			instance.waitForStepResult({
+				name: steps.getStartTime,
+			})
+		).resolves.toBeGreaterThan(0)
+	})
 })
