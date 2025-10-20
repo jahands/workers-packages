df44e2f87eb2eb3572293de302211f5b33616145
Jacob Hands <jacob@jacobhands.com>
2025-10-20 05:56:30 -0500

    chore: add logs to example

---
+++ .changeset/light-banks-type.md
@@ -0,0 +1,5 @@
+---
+'@repo/cron-worker-example': patch
+---
+
+chore: add logs to example
--- examples/cron-worker-example/src/crons/UuidRocksCheckerCron.cron.ts
+++ examples/cron-worker-example/src/crons/UuidRocksCheckerCron.cron.ts
@@ -9,12 +9,14 @@ export class UuidRocksCheckerCron extends CronWorkflow<Env> {
 
 	override async onInit({ step }: CronContext) {
 		await step.do('send sentry checkin', async () => {
+			console.log('cron_on_init')
 			Sentry.captureCheckIn({ monitorSlug: 'uuid-rocks-checker', status: 'in_progress' })
 		})
 	}
 
 	override async onTick({ step }: CronContext) {
 		await step.do('check uuid.rocks', async () => {
+			console.log('cron_on_tick')
 			const res = await fetch('https://uuid.rocks/plain')
 			if (!res.ok) {
 				throw new Error('failed to fetch uuid.rocks/plain')
@@ -25,6 +27,7 @@ export class UuidRocksCheckerCron extends CronWorkflow<Env> {
 
 	override async onFinalize({ step, error }: CronFinalizeContext) {
 		await step.do('send outcome to sentry', async () => {
+			console.log('cron_on_finalize')
 			if (error) {
 				Sentry.captureCheckIn({ monitorSlug: 'uuid-rocks-checker', status: 'error' })
 			} else {
